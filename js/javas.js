072ab3b56e3a55fd0075ffcfafcdd02c36450073
Mane Gamez <manuel173are@gmail.com>
2024-04-24 23:29:38 -0600

    slider, logo, diseño

---
+++ js/javas.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let slides = document.querySelectorAll('.slide');
+    let index = 0;
+    
+    function nextSlide() {
+      slides[index].classList.remove('active');
+      index = (index + 1) % slides.length; // Incrementa el índice o vuelve al inicio
+      slides[index].classList.add('active');
+    }
+    
+    setInterval(nextSlide, 3000); // Cambia cada 3000 milisegundos (3 segundos)
+  });
+  
